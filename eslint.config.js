55a867fa053807e99a01c079d67e29dc0fdc70aa
leon <lza.bupt@gmail.com>
2024-12-18 18:48:44 +0800

    test: init again

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,17 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
   },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { ecmaVersion: 2020, globals: globals.browser } },
   {
     ignores: [
       'coverage',
@@ -24,8 +21,37 @@ export default [
       'pnpm-workspace.yaml',
     ],
   },
+  {
+    files: ['apps/web-app/**/*.{ts,tsx}'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: './apps/web-app',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   eslintPluginPrettierRecommended,
 ];
