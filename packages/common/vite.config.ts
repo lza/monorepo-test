10149fbbb5f86d850196064d4697a2a5ad240683
leon <lza.bupt@gmail.com>
2024-12-19 16:46:50 +0800

    test: monorepo add common pkg

---
+++ packages/common/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: { entry: resolve(__dirname, './src/main.ts'), formats: ['es'] },
+  },
+  resolve: { alias: { src: resolve(__dirname, './src') } },
+  plugins: [dts()],
+});
