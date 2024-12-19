69d34bfdd20905d9bddb1ab3413b9a473879a62a
leon <lza.bupt@gmail.com>
2024-12-19 16:21:02 +0800

    eslint with prettier is ok

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -19,4 +20,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginPrettierRecommended,
 ];
