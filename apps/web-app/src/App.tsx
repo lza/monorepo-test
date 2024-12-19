f5c611839c3e49755956cb06e9a1882acd0e5b3d
leon <lza.bupt@gmail.com>
2024-12-19 15:35:51 +0800

    test: web-app

---
+++ apps/web-app/src/App.tsx
@@ -0,0 +1,10 @@
+import { isEmpty } from 'common';
+
+const App = () => {
+  return (
+    <>
+      <p>undefined isEmpty: {isEmpty(undefined) ? 'true' : 'false'}</p>
+    </>
+  );
+};
+export default App;
