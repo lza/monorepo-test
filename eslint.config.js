1de05870ee7c22a01f4f9469e58184df6844386a
leon <lza.bupt@gmail.com>
2025-01-22 18:41:32 +0800

    test: done

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import { includeIgnoreFile } from '@eslint/compat';
 import { dirname, resolve } from 'path';
@@ -24,9 +26,32 @@ export default [
     ],
   },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { ecmaVersion: 2020, globals: globals.browser } },
+  {
+    files: ['apps/web-app/**/*.{ts,tsx}'],
+    settings: { react: { version: 'detect' } },
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: 'apps/web-app/',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   pluginPrettierRecommended,
 ];
